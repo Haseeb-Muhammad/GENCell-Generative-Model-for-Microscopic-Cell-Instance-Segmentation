b1411164928b1ddf4a50672ba21f3bcef2c922bf
Haseeb-Muhammad <huhammad.bscs22seecs@seecs.edu.pk>
2025-10-06 05:38:13 +0200

    fix: update job script to run the correct training script

--- diffusers_image_generation/job.sh
+++ diffusers_image_generation/job.sh
@@ -8,4 +8,4 @@ srun -K -p A100-80GB,H100,H200,H200-SDS,H100-RP \
   --container-workdir="`pwd`" \
   --mem=64G \
   --container-mounts=/netscratch:/netscratch,"`pwd`":"`pwd`" \
-    /bin/bash -c "bash /netscratch/muhammad/codes/diffusers_image_generation/train_unconditional.py"
+    /bin/bash -c "bash /netscratch/muhammad/codes/diffusers_image_generation/run.sh"
